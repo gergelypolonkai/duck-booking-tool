52e35504271e8ef5108b549eca3f612ea56cee57
Gergely Polonkai <gergely@polonkai.eu>
2014-12-22 14:24:10 +0100

    Add Competence model

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Species(models.Model):
     """Model to hold the Ducks’ species"""
@@ -12,6 +13,16 @@ class Location(models.Model):
     """Model to hold the possible locations of the Ducks"""
 
     name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
+class Competence(models.Model):
+    """Model to hold Duck competences"""
+
+    name = models.CharField(max_length = 255, unique = True)
+    added_at = models.DateTimeField(default = timezone.now)
+    added_by = models.ForeignKey(User)
 
     def __str__(self):
         return self.name
