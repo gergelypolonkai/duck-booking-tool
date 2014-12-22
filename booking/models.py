cd1d99af38506e6b4b2d15ad262e126cdc2264fb
Gergely Polonkai <gergely@polonkai.eu>
2014-12-22 14:52:17 +0100

    Add Location model

--- booking/models.py
+++ booking/models.py
@@ -7,3 +7,11 @@ class Species(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    """Model to hold the possible locations of the Ducks"""
+
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
