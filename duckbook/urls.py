6a20ba1935bf8d8fd9c222fafed6b2dae0cbc99d
Gergely Polonkai <gergely@polonkai.eu>
2014-12-23 09:29:58 +0100

    Add api application skeleton

--- duckbook/urls.py
+++ duckbook/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns(
     '',
     url(r'^admin/',    include(admin.site.urls)),
     url(r'^accounts/', include('accounts.urls', namespace = 'accounts')),
+    url(r'^api/',      include('api.urls',      namespace = 'api')),
     url('',            include('booking.urls',  namespace = 'booking')),
 )
