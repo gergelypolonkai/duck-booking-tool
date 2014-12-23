c7722c418ac1576ab7b0ae3017e0b1a584a7c553
Gergely Polonkai <gergely@polonkai.eu>
2014-12-23 10:39:56 +0100

    Create basic admin site

---
+++ booking/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from booking.models import Species, Location, Competence, Duck, Booking, DuckCompetence, DuckName, DuckNameVote
+
+admin.site.register(Species)
+admin.site.register(Location)
+admin.site.register(Competence)
+admin.site.register(Duck)
+admin.site.register(Booking)
+admin.site.register(DuckCompetence)
+admin.site.register(DuckName)
+admin.site.register(DuckNameVote)
