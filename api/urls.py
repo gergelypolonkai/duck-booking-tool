8fadaba002563004636c19f0f5afde0241b37782
Gergely Polonkai <gergely.polonkai@ericsson.com>
2015-10-19 15:02:37 +0200

    Move api.viewsets to api.views
    Naming is much more clear this way

--- api/urls.py
+++ api/urls.py
@@ -3,25 +3,8 @@ from django.conf.urls import patterns, url, include
 from rest_framework import routers
 
 from . import views
-from . import viewsets
 
 rest_router = routers.DefaultRouter()
-rest_router.register(r'ducks', viewsets.DuckViewSet)
+rest_router.register(r'ducks', views.DuckViewSet)
 
-urlpatterns = patterns(
-    '',
-    url(
-        r'^',
-        include(rest_router.urls)
-    ),
-    url(
-        r'^duck/book/$',
-        views.duck_book,
-        name = 'book'
-    ),
-    url(
-        r'^duck/(?P<duck_id>\d+)/competence.json$',
-        views.DuckCompListView.as_view(),
-        name = 'complist'
-    ),
-)
+urlpatterns = rest_router.urls
