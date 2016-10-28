addfa54b386f437d963d5f9ba20b9418c61a2f4c
Denes Surman <denes.surman@ericsson.com>
2016-10-28 05:45:12 +0200

    Adding base of loginning

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask_login import LoginManager
 
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.add_api('swagger.yaml',
             arguments={
                 'title': 'Rubber Duck Booking Tool'
             })
+
+login_manager = LoginManager()
+#login_manager.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=8080)
