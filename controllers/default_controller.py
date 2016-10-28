2631c344dd111d394e7914d50830e8cf6c4525d9
Gábor Barna <robaganrab@gmail.com>
2016-10-28 11:51:54 +0200

    Add parameters and post handler

--- controllers/default_controller.py
+++ controllers/default_controller.py
@@ -1,6 +1,15 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+from models import Duck
+
+#app =
 def ducks_get():
     return 'do some magic!'
 
-def duck_get():
+def duck_get( duck_id ):
     return 'do some duck magic!'
 
+def duck_post( name, color ):
+    new_duck = Duck( name, color )
+    return new_duck
